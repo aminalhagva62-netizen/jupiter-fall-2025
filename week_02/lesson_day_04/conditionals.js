5a00c52a22351d9d7ed5eee83b58eec9a5b71d60
Jupiter <jupiter@admin-04s-iMac.local>
2025-10-07 21:19:49 +0800

    add: lesson day 04

---
+++ week_02/lesson_day_04/conditionals.js
@@ -0,0 +1,58 @@
+console.log("lesson day")
+
+let name = "dorj";
+let age = 40;
+
+console.log(name);
+console.log(age);
+
+console.log(`minii neriig ${name} 6 bi ${age} nastai6`);
+
+let temperature = 30;
+
+if(temperature > 25 ){
+    console.log (" gadaa haluun baina. nimgen huvtslaarai.");
+
+
+}
+console.log("өдрийг сайхан өнгөрүүлээрэй");
+
+console.log("=====");
+ let userAge = 10;
+
+ if (userAge >= 18){
+    console.log("та насанд хүрсэн байна.");
+ } else{
+    console.log("та насанд хүрээгүй байна.");
+ }
+ console .log("===================")
+  console.log(5==5);
+  console.log("5"==5);
+  console.log("5"===5);
+  console.log( 5 != 5);console.log("a" != "b");
+
+
+  console.log("====================")
+  console.log(1<5);
+  console.log(1>3);
+
+  console.log(7 >= 7);
+  console.log(4 <= 1);
+
+
+
+  console.log("====================");
+
+
+  let trafficLight = "green";
+
+  if(trafficLight == "green"){
+
+  console.log("GO!");
+  } else if (trafficLight == "yellow"){
+    console.log("slow Down! ");
+  } else if (trafficLight == "red"){
+    console.log("STOP!");
+  } else{
+    console.log("invalid color.");  
+}
---
+++ week_02/lesson_day_04/exercise_01.js
@@ -0,0 +1,112 @@
+// let age  = Number(60);
+
+// if (age >= 18){
+//     console.log("Та насанд хүрсэн байна.");
+// }
+// else {
+//     console.log("Та насанд хүрээгүй байна.");
+// }
+
+
+
+
+
+// let number = Number("-8");
+
+// if (number > 0){
+//     console.log("Энэ бол эерэг тоо.");
+// }
+// else if (number < 0){
+//     console.log("Энэ бол сөрөг тоо.");
+// }
+// else {
+//     console.log("Энэ бол тэг.");
+// }
+
+
+// let number = 9;
+
+// if ( number % 2 == 0){
+// console.log("сондгой  тоо");
+// } else{
+//     console.log("тэгш   тоо");
+//  }
+
+
+
+// let userName = "amina";
+// let passWord = "1234";
+// if (userName == "bold" && passWord == "1234") {
+//     console.log("амжилттай нэвтэрлээ .");
+// } else {
+//     console.log("нэр эсвэл нууц үг буруу байна. ");
+// }
+
+
+
+// let temp = 5;
+// let is_raining = "ugui";
+// if (temp > 15 && is_raining == "tiim"){
+//     console.log("дулаан хувцаслаж  шүхрээ аваарай.");
+// } else if (temp < 15){
+//     console.log("шүхэргүй явсан ч болно. ");
+// }
+// else{
+//     console.log(" өөрөө мэд ")
+// }
+
+
+
+
+// let day = 5;
+// if (day == "бямба" && day == "ням"){
+//     console.log("амралтын өдрийн мэнд ");  
+// }else{
+//         console.log(" ажлын өдөр байна амжилт")
+// }
+
+    
+
+
+// let score = 70;
+// if(score >= 90){
+//     console.log("A");
+// }else{
+//     console.log("-A");
+// }
+//   if(score >= 80){
+ 
+//     console.log("B");
+// }else{
+//     console.log("-B")}
+
+
+
+// let age = 15;
+// if (age < 12){
+//     console.log("price=500")
+// }else{
+//     console.log("price=9000")
+// }
+// if (age < 65){
+//     console.log("price=10000")
+// }else{
+//     console.log("price=7000")
+// }
+
+
+
+
+let a = 2
+let b = 3
+let c = 5
+if (a == b && b == c){
+    console.log("адил талт ")
+}else{
+    console.log("адил талт бус ")
+}
+if (a == b && b == c && a == c){
+    console.log("адил хажуут  ")
+}else{
+    console.log("зөрөө  талт")
+}
